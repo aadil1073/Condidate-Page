f6827d486ef04403dbf7782cfc219cc9e987ae31
aadil1073 <98414845+aadil1073@users.noreply.github.com>
2025-01-25 15:35:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Placeholder for form submission functionality
+document.querySelector('.contact-form').addEventListener('submit', event => {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+  });
+  
